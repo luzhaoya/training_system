c6cd832a3d532f782da66675ae14bf612423578d
luzhao <luzhaoya@qq.com>
2023-04-15 12:38:32 +0800

    feat: 新增企业入职时审核的表

---
+++ src/main/java/com/example/training_system/pojo/EnterpriseCheck.java
@@ -0,0 +1,14 @@
+package com.example.training_system.pojo;
+
+/**
+ * 作用：用于学校审核企业入驻
+ * @author luzhao
+ * @date 2023/04/15
+ */
+public class EnterpriseCheck {
+    private Integer id;
+    private String enterpriseId;
+
+    private String enterpriseName;
+    private String condition;
+}
