3a70ffea341422aa64833e7ad1f0308664daa5a6
wangtieliang <2050134213@qq.com>
2023-04-15 14:42:58 +0800

    新增接口

---
+++ src/main/java/com/example/training_system/service/accountManagement/AccountManagement.java
@@ -0,0 +1,9 @@
+package com.example.training_system.service.accountManagement;
+
+/**
+ * 账号管理
+ * @author wangtieliang
+ * @since 2023-04-15
+ */
+public interface AccountManagement {
+}
---
+++ src/main/java/com/example/training_system/service/accountManagement/impl/AccountManagementImpl.java
@@ -0,0 +1,11 @@
+package com.example.training_system.service.accountManagement.impl;
+
+import com.example.training_system.service.accountManagement.AccountManagement;
+
+/**
+ * 账号管理
+ * @author wangtieliang
+ * @since 2023-04-15
+ */
+public class AccountManagementImpl implements AccountManagement {
+}
