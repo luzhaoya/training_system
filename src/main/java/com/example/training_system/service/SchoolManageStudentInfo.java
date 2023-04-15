5ea1ab73f80436bc78fadf90aeecb070811abaee
luzhao <luzhaoya@qq.com>
2023-04-15 12:01:16 +0800

    feat: 新增数据源配置

--- src/main/java/com/example/training_system/service/SchoolManageStudentInfo.java
+++ src/main/java/com/example/training_system/service/SchoolManageStudentInfo.java
@@ -1,4 +1,9 @@
 package com.example.training_system.service;
 
+/**
+ * 学校管理学生信息
+ * @author luzhao
+ * @date 2023/04/15
+ */
 public interface SchoolManageStudentInfo {
 }
--- src/main/java/com/example/training_system/service/SchoolStudentEmployment.java
+++ src/main/java/com/example/training_system/service/SchoolStudentEmployment.java
@@ -1,4 +1,9 @@
 package com.example.training_system.service;
 
+/**
+ * 学校学生工作跟踪
+ * @author luzhao
+ * @date 2023/04/15
+ */
 public interface SchoolStudentEmployment {
 }
