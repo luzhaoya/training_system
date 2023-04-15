143eabfc682e10ef9ede7c10bc476bf20d9255c2
luzhao <luzhaoya@qq.com>
2023-04-15 17:53:01 +0800

    feat: 实体类新建完毕

--- src/main/java/com/example/training_system/pojo/SyllabusInfo.java
+++ src/main/java/com/example/training_system/pojo/SyllabusInfo.java
@@ -1,14 +1,34 @@
 package com.example.training_system.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.example.training_system.annotate.FieldInfo;
+
 import java.lang.Integer;
 import java.lang.String;
 
+/**
+ * 课程信息实体
+ * @author luzhao
+ * @date 2023/04/15
+ */
+@TableName("t_syllabus_info")
 public class SyllabusInfo {
+	@TableId(value = "id", type = IdType.AUTO)
 	private Integer id;
-	private String courseContent;
-	private String courseName;
-	private Integer enterpriseId;
+
+	@FieldInfo("课程名称")
 	private String enterpriseName;
+
+	@FieldInfo("课程内容")
+	private String courseContent;
+
+	@FieldInfo("企业id")
+	private String courseName;
+
+	@FieldInfo("企业名称")
+	private Integer enterpriseId;
 
 	public Integer getId() {
 		return id;
