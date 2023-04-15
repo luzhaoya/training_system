c85b3d34124394d7e1fc7cadea5a98e5c70e3683
luzhao <luzhaoya@qq.com>
2023-04-15 08:48:58 +0800

    frist commit

---
+++ src/main/java/com/example/training_system/TrainingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.training_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingSystemApplication.class, args);
+    }
+
+}
