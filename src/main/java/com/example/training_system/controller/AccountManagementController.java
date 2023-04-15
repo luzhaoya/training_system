023c64fc7eb38aab130f85c0243b449295f1133d
wangtieliang <2050134213@qq.com>
2023-04-15 17:58:30 +0800

    新增接口

--- src/main/java/com/example/training_system/controller/AccountManagementController.java
+++ src/main/java/com/example/training_system/controller/AccountManagementController.java
@@ -1,19 +1,20 @@
 package com.example.training_system.controller;
 
 import com.example.training_system.pojo.Account;
-import com.example.training_system.pojo.Hello;
 import com.example.training_system.service.accountManagement.AccountManagement;
 import com.example.training_system.util.JsonUtil;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpServletResponseWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * 账号管理
@@ -25,25 +26,30 @@ import java.net.http.HttpResponse;
 public class AccountManagementController {
     @Autowired
     private AccountManagement accountManagement;
-    @RequestMapping(value = "/login", produces = "application/json; charset=utf-8")
+    @PostMapping(value = "/login", produces = "application/json; charset=utf-8")
     public String login(@RequestBody Account account, HttpServletResponse res) {
-        System.err.println(account.getAccount());
         var login = accountManagement.login(account);
         if (null != login){
             Cookie id = new Cookie("id", String.valueOf(login.getId()));
+            Cookie role = new Cookie("role", login.getRole());
+            Cookie entityId = new Cookie("entityId", login.getEntityId());
             id.setHttpOnly(true);
             id.setMaxAge(6000);
-            Cookie role = new Cookie("role", login.getRole());
             role.setHttpOnly(true);
             role.setMaxAge(6000);
-            Cookie entityId = new Cookie("entityId", String.valueOf(login.getEntityId()));
             entityId.setHttpOnly(true);
             entityId.setMaxAge(6000);
-
             res.addCookie(id);
             res.addCookie(role);
             res.addCookie(entityId);
         }
+
         return JsonUtil.get().assemble(login);
     }
+
+    @PostMapping(value = "/register", produces = "application/json; charset=utf-8")
+    public String register(@RequestBody Account account){
+        return JsonUtil.get().assemble(accountManagement.register(account));
+    }
+
 }
